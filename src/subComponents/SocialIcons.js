4b1bce22a8f35bf81616c8ea45d2a962208d656f
harrynmt <harris.ibrahim024@gmail.com>
2024-12-16 15:18:06 +0530

    portfolio final deploy

--- src/subComponents/SocialIcons.js
+++ src/subComponents/SocialIcons.js
@@ -1,6 +1,6 @@
 import { motion } from "framer-motion";
 import React from "react";
-// import { NavLink } from 'react-router-dom'
+// import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 import { Facebook, Github, Twitter, YouTube } from "../components/AllSvgs";
 import { DarkTheme } from "../components/Themes";
